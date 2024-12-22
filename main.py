0115800307237df1951e3f78976bd9b5aced5f3c
Noam <noamgit@gmail.com>
2024-12-22 10:02:28 +0200

    fix: deprecated distutils, use shutil

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import json
 import logging
 import shlex
-import distutils.spawn
+import shutil
+
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
@@ -18,11 +19,27 @@ logger = logging.getLogger(__name__)
 global usage_cache
 usage_cache = {}
 
+
+def find_executable(executable_name):
+    # Use shutil.which if available (Python 3.3+)
+    if hasattr(shutil, "which"):
+        return shutil.which(executable_name)
+    try:
+        from distutils import spawn
+
+        return spawn.find_executable(executable_name)
+    except ImportError:
+        return None
+
+
+# Example usage
+executable_path = find_executable("remmina")
+
 # Usage tracking
 script_directory = os.path.dirname(os.path.realpath(__file__))
 usage_db = os.path.join(script_directory, "usage.json")
 if os.path.exists(usage_db):
-    with open(usage_db, 'r') as db:
+    with open(usage_db, "r") as db:
         # Read JSON string
         raw = db.read()
         # JSON to dict
@@ -31,11 +48,11 @@ if os.path.exists(usage_db):
 # Initialize items cache and Remmina profiles path
 remmina_bin = ""
 # Locate Remmina profiles and binary
-default_paths = ["{}/.local/share/remmina".format(os.environ.get('HOME')),
-                 "{}/.remmina".format(os.environ.get('HOME'))]
-# remmina_profiles_path = "{}/.local/share/remmina".format(os.environ.get('HOME'))
-# remmina_profiles_path_alt = "{}/.remmina".format(os.environ.get('HOME'))
-remmina_bin = distutils.spawn.find_executable('remmina')
+default_paths = [
+    "{}/.local/share/remmina".format(os.environ.get("HOME")),
+    "{}/.remmina".format(os.environ.get("HOME")),
+]
+remmina_bin = find_executable("remmina")
 # This extension is useless without remmina
 if remmina_bin is None or remmina_bin == "":
     logger.error("Remmina executable path could not be determined")
@@ -75,10 +92,11 @@ class RemminaExtension(Extension):
             # Multiple strings can be used to search in description
             # all() is used to achieve a AND search (include all keywords)
             keywords = query.split(" ")
-            # if (query in base.lower()) or (query in desc.lower()):
-            if (query.lower() in base.lower()) or \
-               (query.lower() in title.lower()) or \
-               all(x.lower() in desc.lower() for x in keywords):
+            if (
+                (query.lower() in base.lower())
+                or (query.lower() in title.lower())
+                or all(x.lower() in desc.lower() for x in keywords)
+            ):
                 items_cache.append(create_item(title, proto, p, desc, p))
 
         items_cache = sorted(items_cache, key=sort_by_usage, reverse=True)
@@ -88,11 +106,11 @@ class RemminaExtension(Extension):
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         global remmina_profiles_path
-        if extension.preferences["profiles"] != "" \
-           or not remmina_profiles_path:
+        if extension.preferences["profiles"] != "" or not remmina_profiles_path:
             # Tilde (~) won't work alone, need expanduser()
-            remmina_profiles_path = os.path.expanduser(extension.preferences["profiles"])
-        # pref_profiles_path = extension.preferences['profiles']
+            remmina_profiles_path = os.path.expanduser(
+                extension.preferences["profiles"]
+            )
         logger.debug("Remmina profiles path: {}".format(remmina_profiles_path))
         # Get query
         term = (event.get_argument() or "").lower()
@@ -112,30 +130,32 @@ class ItemEnterEventListener(EventListener):
         b = os.path.splitext(base)[0]
         # Check usage and increment
         if b in usage_cache:
-            usage_cache[b] = usage_cache[b]+1
+            usage_cache[b] = usage_cache[b] + 1
         else:
             usage_cache[b] = 1
         # Update usage JSON
-        with open(usage_db, 'w') as db:
+        with open(usage_db, "w") as db:
             db.write(json.dumps(usage_cache, indent=2))
-        return RunScriptAction('#!/usr/bin/env bash\n{} -c {}\n'.format(remmina_bin, shlex.quote(on_enter)), None).run()
+        return RunScriptAction(
+            "#!/usr/bin/env bash\n{} -c {}\n".format(
+                remmina_bin, shlex.quote(on_enter)
+            ),
+            None,
+        ).run()
 
 
 def create_item(name, icon, keyword, description, on_enter):
     return ExtensionResultItem(
-            name=name,
-            description=description,
-            keyword=keyword,
-            icon="images/{}.svg".format(icon),
-            on_enter=ExtensionCustomAction(
-                 {"id": on_enter})
-            )
+        name=name,
+        description=description,
+        keyword=keyword,
+        icon="images/{}.svg".format(icon),
+        on_enter=ExtensionCustomAction({"id": on_enter}),
+    )
 
 
 def sort_by_usage(i):
     global usage_cache
-    # Convert item name to ID format
-    # j = i._name.lower()
     base = os.path.basename(i._keyword)
     j = os.path.splitext(base)[0]
     # Return score according to usage
@@ -185,16 +205,13 @@ def profile_details(profile_path):
                 else:
                     pass
             if len(username) > 0:
-                server = "{username}@{server}".format(username=username,
-                                                      server=server)
+                server = "{username}@{server}".format(username=username, server=server)
             if len(proto) > 0:
-                server = "{proto}://{server}".format(proto=proto,
-                                                     server=server)
+                server = "{proto}://{server}".format(proto=proto, server=server)
             if len(group) > 0:
                 group = " | {group}".format(group=group)
             # Final description string
-            desc = "{server} {group}".format(server=server,
-                                             group=group)
+            desc = "{server} {group}".format(server=server, group=group)
             return name, desc, proto
     else:
         # Default values
